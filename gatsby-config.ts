8bda6b6087913ab4c817a29e4e4a9b87870814d9
heejihn <heejihn@hotmail.com>
2023-03-02 00:07:21 +0900

    デプロイ設定修正

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `tc-website`,
     siteUrl: `https://tc-online.tik-choco.com`
   },
+  pathPrefix: `/tc-website`,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
