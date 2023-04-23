ad358dab1ba27243cc8903bb9d3672c3588c789e
fog <38463346+fog-zs@users.noreply.github.com>
2023-04-23 18:31:41 +0900

    Update: Changed News component to read from JSON file

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,32 +1,33 @@
 import { Button, Card, CardActionArea, CardActions, CardContent, Grid, Typography } from "@mui/material"
 import React from "react"
 import NewsCard from "./NewsCard"
+import NewsJsonData from "../../../content/news.json"
 
-const News = () => {
+type Content = {
+    date: string
+    title: string
+    url: string
+    image: string
+}
+
+type Post = {
+    posts: Content[]
+}
+
+const News = () => {    
+    const NewsData: Post = NewsJsonData
+
     return (<>
         <Grid container spacing={4}>
-            <NewsCard
-                date="2023/4/15"
-                title="開発者募集中"
-                url="https://game-creators.camp/recruits/qYAy4wQmEzc6bymBdoeB"
-            />
-            <NewsCard
-                date="2023/3/18"
-                title="第2回 TikChocoConnect 公開"
-                image="https://i.ytimg.com/vi/a2QT-10UuM8/hqdefault.jpg?"
-                url="https://youtu.be/a2QT-10UuM8"
-            />
-            <NewsCard
-                date="2023/2/19"
-                title="第1回 TikChocoConnect 公開"
-                image="https://i.ytimg.com/vi/kJFBjG2eSuk/hqdefault.jpg"
-                url="https://youtu.be/kJFBjG2eSuk"
-            />
-            <NewsCard
-                date="2023/2/4"
-                title="ホームページ公開"
-                description="詳細"
-            />
+            {NewsData.posts.map((post, index) => (
+                <NewsCard
+                    key={index}
+                    date={post.date}
+                    title={post.title}
+                    url={post.url}
+                    image={post.image}
+            />    
+            ))}            
         </Grid>
     </>)
 }
